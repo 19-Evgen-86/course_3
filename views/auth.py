1a4370b399c1ead132383b91e799fcd321eb270c
evgeniy <ninjago58rus@gmail.com>
2022-06-16 15:53:58 +0300

    update

--- views/auth.py
+++ views/auth.py
@@ -13,7 +13,7 @@ user = auth_ns.model('user', convert_model(UserSchema()))
 @auth_ns.route("/register/")
 # регистрация пользователя
 class AuthView(Resource):
-    @auth_ns.expect(user)
+
     @auth_ns.response(200, description="Пользователь создан ")
     @auth_ns.response(404, description="Пользователь существует ")
     def post(self):
@@ -22,7 +22,7 @@ class AuthView(Resource):
 @auth_ns.route("/login/")
 class AuthView(Resource):
     # авторизация пользователя
-    @auth_ns.expect(user)
+
     @auth_ns.response(200, description="Авторизация успешна")
     @auth_ns.response(404, description="Ошибка авторизации ")
     def post(self):
